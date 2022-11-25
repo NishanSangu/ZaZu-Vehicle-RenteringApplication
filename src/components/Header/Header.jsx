df1a90f03d674b6e79bf04bf7c2ff66147975d88
piriyaraj <piriyaraj1998@gmail.com>
2022-11-25 16:36:48 +0530

    added login and signup form

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -43,18 +43,18 @@ const Header = () => {
               <div className="header__top__left">
                 <span>Need Help?</span>
                 <span className="header__top__help">
-                  <i class="ri-phone-fill"></i> +940764646229
+                  <i class="ri-phone-fill"></i> +94xxxxxxxxx
                 </span>
               </div>
             </Col>
 
             <Col lg="6" md="6" sm="6">
               <div className="header__top__right d-flex align-items-center justify-content-end gap-3">
-                <Link to="#" className=" d-flex align-items-center gap-1">
+                <Link to="/login" className=" d-flex align-items-center gap-1">
                   <i class="ri-login-circle-line"></i> Login
                 </Link>
 
-                <Link to="#" className=" d-flex align-items-center gap-1">
+                <Link to="/signup" className=" d-flex align-items-center gap-1">
                   <i class="ri-user-line"></i> Register
                 </Link>
               </div>
